2f9075faad9bab514c0e75646af4efe95235e671
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2024-12-01 23:20:29 +0530

    Array operations

---
+++ Functional Programming/reduce.js
@@ -0,0 +1,5 @@
+const num = [3,5,61,6];
+const sum = num.reduce((acc,value) => (acc + value), 0)
+// The reduce() method applies a provided function to each array element in order to reduce it to one value. This method is typically used to perform calculations on an array.
+console.log(num)
+console.log(sum)
