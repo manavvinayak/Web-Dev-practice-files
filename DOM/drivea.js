8f0d7c5c4ba83cac21b9577907703ed074510ba4
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-02-27 03:46:20 +0530

    prompt

---
+++ DOM/drivea.js
@@ -0,0 +1,18 @@
+let age = prompt("enter your age")
+age = Number.parseInt(age)
+let runAgain = true;
+
+while(runAgain) {
+    let age = prompt("enter your age")
+     age = Number.parseInt(age)
+     const candrive =(age) => {
+     return age >= 18?true:false
+    
+}
+if(candrive(age)){
+    alert("Yes you can drive")
+}
+else{
+    alert("You cant drive")
+}
+}
