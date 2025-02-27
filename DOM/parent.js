7bd33dfad00b24b3c68c63f9e222d708669763c2
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-02-28 02:46:20 +0530

    element

---
+++ DOM/parent.js
@@ -0,0 +1,4 @@
+console.log(document.body.firstChild); // Accessing the first child of the body element
+var a = document.body.firstChild; // Assigning the first child of the body to variable a
+console.log(a.parentNode); // Logging the parent node of the first child
+console.log(a.parentElement); // Logging the parent element of the first child
