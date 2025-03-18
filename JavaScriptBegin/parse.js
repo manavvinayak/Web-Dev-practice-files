0e20c5a0c40244708819d790788c22128c9a217c
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-03-18 21:55:06 +0530

    Init

--- JavaScriptBegin/parse.js
+++ JavaScriptBegin/parse.js
@@ -3,7 +3,13 @@ function explainParseint(value) {
     let result = parseInt(value);
     console.log(result)
 }
+function explainFloatint(value) {
+    console.log("original value", value)
+    let result = parseFloat(value);
+    console.log(result)
+}
 
-explainParseint("34")
+explainParseint("34.8")
 explainParseint("fgehgf")
 explainParseint("45px")
+explainFloatint("45.88")
