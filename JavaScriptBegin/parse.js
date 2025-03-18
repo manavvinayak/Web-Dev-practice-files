f105eb6354564869da4f4f833adac3b30be87f91
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-03-18 11:54:34 +0530

    revisit

---
+++ JavaScriptBegin/parse.js
@@ -0,0 +1,9 @@
+function explainParseint(value) {
+    console.log("original value", value)
+    let result = parseInt(value);
+    console.log(result)
+}
+
+explainParseint("34")
+explainParseint("fgehgf")
+explainParseint("45px")
