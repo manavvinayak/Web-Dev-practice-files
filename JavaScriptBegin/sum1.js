786bfbde224081a0c3d6b8ecc65cad1f09234d36
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-03-21 05:34:12 +0530

    revisit

--- JavaScriptBegin/sum1.js
+++ JavaScriptBegin/sum1.js
@@ -1,11 +1,16 @@
 // find sum from 1 to a number
 function sum(n) {
-    let ans = 0;
-    for(let i=1; i<= n; i++) {
-        ans = ans + i;
-    }
-    return ans;
+    // let ans = 0;
+    // // for(let i=1; i<= n; i++) {
+    // //     ans = ans + i;
+    // // }
+
+    // return ans;
+    return n * (n+1);
 }
 const ans = sum(33)
 // sum will be 1 + ......33
-console.log(ans)
+console.log(ans);
+const ans2 = sum(43)
+console.log(ans2);
+// synchronous codes run in line by line format 
