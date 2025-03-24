d91d5113a8d744fba3133ed153f1388edd8337f8
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-03-24 05:51:00 +0530

    js

---
+++ objectsjs/main.js
@@ -0,0 +1,16 @@
+const personArray = ["hark","remen","priya"]
+const persongender = ["male","male","female"]
+const number = personArray.length
+const user1 = {
+    firstname:"vinayak",
+    gender:"male"
+}
+// accessing key
+user1.firstname;
+
+const movies = {
+    name: "tenet",
+    year:'2009',
+    type:"sci fi"
+}
+console.log(movies.name)
